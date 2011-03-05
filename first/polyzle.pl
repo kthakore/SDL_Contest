560d6144dc4f3a451c670ae406ebaa8d27270a06
kthakore <thakore.kartik@gmail.com>
2011-03-05 15:52:42 -0500

    Added data icon

--- first/polyzle.pl
+++ first/polyzle.pl
@@ -9,8 +9,8 @@ use lib 'lib';
 use Polygon;
 
 
-my $surface = SDLx::App->new( eoq=> 1, depth => 32, flags => SDL_HWSURFACE | SDL_DOUBLEBUF, delay => 60 );
+my $app = SDLx::App->new( title => "Polygon Trouble", eoq=> 1, depth => 32, flags => SDL_HWSURFACE | SDL_DOUBLEBUF, delay => 60, icon => 'data/icon.bmp' );
 my $tri = Polygon->new( verts => 5, color => 0xff00ffff, bgcolor => 0x00000011, radius => 20, width => 200, height => 200, depth => 32 );
 
-$tri->attach( app => $surface );
-$surface->run();
+$tri->attach( app => $app );
+$app->run();
